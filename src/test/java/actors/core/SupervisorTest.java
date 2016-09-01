93ffb9ff1f8cef1baf8e83b960933e883e7a928f
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2016-09-01 20:02:50 +0200

    Test send unknown message to Supersivor actor.

---
+++ src/test/java/actors/core/SupervisorTest.java
@@ -0,0 +1,59 @@
+package actors.core;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.testkit.JavaTestKit;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * Created by zua on 01.09.16.
+ */
+public class SupervisorTest extends AbstractActorTest {
+
+    //@Test(dataProvider = "dataProviderOnReceive")
+    public void testSupervisorStrategy(Object message) throws Exception {
+        new JavaTestKit(getActorSystem()) {
+            {
+                // TODO: Not Yet Implemented
+            }
+        };
+
+    }
+
+    @Test(dataProvider = "dataProvider")
+    public void testOnReceive(Object message) throws Exception {
+        new JavaTestKit(getActorSystem()) {
+            {
+                ActorRef supervisor = getActorSystem().actorOf(Props.create(Supervisor.class), "Supervisor");
+                supervisor.tell(message, getRef());
+                ActorRef child = expectMsgClass(ActorRef.class);
+                assertNotNull(child);
+            }
+        };
+    }
+
+    @Override
+    public void testUnhandled() {
+        new JavaTestKit(getActorSystem()) {
+            {
+                ActorRef supervisor = getActorSystem().actorOf(Props.create(Supervisor.class), "Supervisor");
+                supervisor.tell("Hello", getRef());
+                expectNoMsg();
+            }
+        };
+    }
+
+
+    // DATA PROVIDER METHODS
+    @DataProvider(name = "dataProvider")
+    public Object[][] dataProviderOnReceive() {
+        return new Object[][]{
+                {Props.create(WelcomeActor.class)},
+                {Props.create(RegisterActor.class)},
+                {Props.create(LoginActor.class)}
+        };
+    }
+}
